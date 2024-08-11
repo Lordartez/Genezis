6ffe4d2f5e11447d20bd4991e4a0a876a99f1922
Artem Kozlov <103585435+Lordartez@users.noreply.github.com>
2024-08-11 16:04:43 +0300

    Delete RobustToolbox directory

--- RobustToolbox/Robust.Shared/GameObjects/EntitySystemMessages/EntityRenamedEvent.cs
+++
@@ -1,7 +0,0 @@
-namespace Robust.Shared.GameObjects;
-
-/// <summary>
-/// Raised directed on an entity when its name is changed.
-/// </summary>
-[ByRefEvent]
-public readonly record struct EntityRenamedEvent(string NewName);
