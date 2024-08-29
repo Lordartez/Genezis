bbc734a646eebc3fe35ccc60dbe337ab294c7183
Artem Kozlov <lordartez2@mail.ru>
2024-08-29 04:23:50 +0300

    Чистка файлов и ретекстуринг

--- Content.Shared/_RMC14/CCVar/CMCVars.cs
+++ Content.Shared/_RMC14/CCVar/CMCVars.cs
@@ -51,42 +51,4 @@ public sealed class CMCVars : CVars
     public static readonly CVarDef<float> CMBleedTimeMultiplier =
         CVarDef.Create("cm.bleed_time_multiplier", 1f, CVar.REPLICATED | CVar.SERVER);
 
-    public static readonly CVarDef<float> CMMarinesPerXeno =
-        CVarDef.Create("cm.marines_per_xeno", 3.5f, CVar.REPLICATED | CVar.SERVER);
-
-    public static readonly CVarDef<bool> RMCAutoBalance =
-        CVarDef.Create("rmc.auto_balance", true, CVar.SERVER);
-
-    public static readonly CVarDef<float> RMCAutoBalanceStep =
-        CVarDef.Create("rmc.auto_balance_step", 0.25f, CVar.SERVER);
-
-    public static readonly CVarDef<float> RMCAutoBalanceMin =
-        CVarDef.Create("rmc.auto_balance_min", 3f, CVar.SERVER);
-
-    public static readonly CVarDef<float> RMCAutoBalanceMax =
-        CVarDef.Create("rmc.auto_balance_max", 4.5f, CVar.SERVER);
-
-    public static readonly CVarDef<int> RMCPatronLobbyMessageTimeSeconds =
-        CVarDef.Create("rmc.patron_lobby_message_time_seconds", 30, CVar.REPLICATED | CVar.SERVER);
-
-    public static readonly CVarDef<int> RMCPatronLobbyMessageInitialDelaySeconds =
-        CVarDef.Create("rmc.patron_lobby_message_initial_delay_seconds", 5, CVar.REPLICATED | CVar.SERVER);
-
-    public static readonly CVarDef<string> RMCDiscordAccountLinkingMessageLink =
-        CVarDef.Create("rmc.discord_account_linking_message_link", "", CVar.REPLICATED | CVar.SERVER);
-
-    public static readonly CVarDef<int> RMCRequisitionsBalanceGain =
-        CVarDef.Create("rmc.requisitions_balance_gain", 300, CVar.REPLICATED | CVar.SERVER);
-
-    public static readonly CVarDef<string> RMCDiscordToken =
-        CVarDef.Create("rmc.discord_token", "", CVar.SERVER | CVar.SERVERONLY | CVar.CONFIDENTIAL);
-
-    public static readonly CVarDef<ulong> RMCDiscordAdminChatChannel =
-        CVarDef.Create("rmc.discord_admin_chat_channel", 0UL, CVar.SERVER | CVar.SERVERONLY | CVar.CONFIDENTIAL);
-
-    /// <summary>
-    ///     Comma-separated list of maps to load as the planet in the distress signal gamemode.
-    /// </summary>
-    public static readonly CVarDef<string> RMCPlanetMaps =
-        CVarDef.Create("rmc.planet_maps", "/Maps/_RMC14/lv624.yml", CVar.SERVER | CVar.SERVERONLY);
 }
